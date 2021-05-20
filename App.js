73197e389ef47aa1da50f2a2eafbea3faac73466
Nick Howard <nick@revieve.com>
2021-05-20 11:29:30 +0300

    Fix camera access for Android
    * Add additional permissions
    * Configure media playback and userAgent on webview
    * Retrieve the user agent from the device

--- App.js
+++ App.js
@@ -2,31 +2,54 @@
  * Sample React Native App integrating Revieve framework in a webview
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
+  StyleSheet,
   useColorScheme,
   View,
 } from 'react-native';
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
+
+const styles = StyleSheet.create({
+  fullHeight: {
+    height: '100%',
+  },
+});
 
 const App = () => {
+  const [userAgent, setUserAgent] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // get the userAgent from the device
+      const deviceAgent = await DeviceInfo.getUserAgent();
+      // Having wv in the user agent string stops getUserMedia from working. Could do with finding some more info as to what is causing this.
+      setUserAgent(deviceAgent.replace(/; ?wv/g, ''));
+    })();
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
+
+  // only render when we have a userAgent
+  if (!userAgent) {
+    return null;
+  }
 
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View
-        style={{
-          height: "100%",
-        }}>
+      <View style={styles.fullHeight}>
         <WebView
-          source={{ uri: 'https://demov4.revieve.com' }}
+          source={{uri: 'https://demov4.revieve.com'}}
           automaticallyAdjustContentInsets={false}
           geolocationEnabled={true}
           javaScriptEnabled={true}
           allowsInlineMediaPlayback={true}
+          mediaPlaybackRequiresUserAction={false}
+          userAgent={userAgent}
         />
       </View>
     </SafeAreaView>
